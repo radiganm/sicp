999ad248d3dd6791f662e71247f4c6ef81e22089
mac <mac@radigan.org>
2016-11-07 02:57:11 -0800

    update

---
+++ chapter-1/sicp_ch1_e1-10.scm
@@ -0,0 +1,46 @@
+#!/usr/bin/csi -s
+;; sicp_ch1_e1-10.scm
+;; Mac Radigan
+
+  (load "../library/util.scm")
+  (import util)
+
+;; Exercise 1.10: The following procedure computes a mathematical function called Ackermann's function.
+;;
+;;   (define (A x y)
+;;     (cond ((= y 0) 0)
+;;       ((= x 0) (* 2 y))
+;;       ((= y 1) 2)
+;;     (else (A (- x 1) (A x (- y 1))))))
+
+;; What are the values of the following expressions?
+;;   (A 1 10)
+;;   (A 2 4)
+;;   (A 3 3)
+;;
+;; Consider the following procedures, where A is the procedure defined above:
+;;   (define (f n) (A 0 n))
+;;   (define (g n) (A 1 n))
+;;   (define (h n) (A 2 n))
+;;   (define (k n) (* 5 n n))
+
+;; Give concise mathematical definitions for the functions computed by the procedures f , g , and h for positive integer values of n. For example, (k n) computes 5n 2 .
+
+  (define (A x y)
+    (cond ((= y 0) 0)
+      ((= x 0) (* 2 y))
+      ((= y 1) 2)
+    (else (A (- x 1) (A x (- y 1))))))
+
+  (bar)
+  (prnvar "(A 1 10)" (A 1 10))
+  (prnvar "(A 2 4)"  (A 2  4))
+  (prnvar "(A 3 3)"  (A 3  3))
+  (hr)
+  (prn "(f n) := 2*n")
+  (prn "(g n) := 2")
+  (prn "(h n) := ")
+  (prn "(k n) := 5*n^2")
+  (bar)
+
+;; *EOF*
